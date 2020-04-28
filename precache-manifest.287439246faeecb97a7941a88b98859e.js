7c947db91cc13f7689f620c6799a81688a059d8f
IamTheReality <iamthereality@yandex.ru>
2020-04-29 01:53:16 +0300

    Updates

---
+++ precache-manifest.287439246faeecb97a7941a88b98859e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4414e51c9a43f162c4ccd0707a9d03aa",
+    "url": "/db_star_wars/index.html"
+  },
+  {
+    "revision": "b4cce44a14641ae7eff9",
+    "url": "/db_star_wars/static/css/main.a7df6515.chunk.css"
+  },
+  {
+    "revision": "a81c254fe6caaa85d937",
+    "url": "/db_star_wars/static/js/2.b6b60d51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/db_star_wars/static/js/2.b6b60d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4cce44a14641ae7eff9",
+    "url": "/db_star_wars/static/js/main.c84c7c26.chunk.js"
+  },
+  {
+    "revision": "0e2f93467483b37e8bf4",
+    "url": "/db_star_wars/static/js/runtime-main.a94530a7.js"
+  },
+  {
+    "revision": "2c7d2081f91db6d2dd8629eff48bb19a",
+    "url": "/db_star_wars/static/media/Planet_Placeholder.2c7d2081.jpg"
+  }
+]);
